e6c3f91289f827b753ed78fe7d16a32e31121468
Harutyun Shamyan <shamyan.harut@gmail.com>
2022-07-31 12:37:06 +0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Harutyun Shamyan on 31.07.22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
